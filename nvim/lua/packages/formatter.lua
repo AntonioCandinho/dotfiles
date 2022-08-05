21ae090d36448320e4b30df74a0befc2cd983647
Antonio Candinho <antonio@goodnotesapp.com>
2022-08-05 12:55:23 +0200

    Configure lua formatter

--- nvim/lua/packages/formatter.lua
+++ nvim/lua/packages/formatter.lua
@@ -40,7 +40,7 @@ end
 M.stylua = function()
   return {
     exe = "stylua",
-    args = { "--search-parent-directories", "--stdin-filepath", '"%:p"', "--", "-" },
+    args = { "--search-parent-directories", "--stdin-filepath", vim.api.nvim_buf_get_name(0), "--", "-" },
     stdin = true,
   }
 end
