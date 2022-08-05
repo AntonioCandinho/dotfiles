dca3142ab885a4d704e41005d554c1c7cc914d72
Antonio Candinho <antonio@goodnotesapp.com>
2022-08-05 13:10:33 +0200

    Replace rnvim with netrw

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -72,8 +72,6 @@ function M.apply()
   vim.g.loaded_2html_plugin = 1
   vim.g.loaded_logiPat = 1
   vim.g.loaded_rrhelper = 1
-  vim.g.loaded_netrw = 1
-  vim.g.loaded_netrwPlugin = 1
   vim.g.loaded_netrwSettings = 1
   vim.g.loaded_netrwFileHandlers = 1
   vim.g.loaded_man = 1
