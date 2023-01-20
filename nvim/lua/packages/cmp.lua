452ad469194929569cb9e1f1fcbea78bd6626345
Antonio Candinho <antonio@goodnotesapp.com>
2023-01-20 13:25:44 +0100

    Update cmp config

--- nvim/lua/packages/cmp.lua
+++ nvim/lua/packages/cmp.lua
@@ -1,12 +1,6 @@
 local cmp = require "cmp"
 local M = {}
 
-local has_words_before = function()
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0
-    and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
-end
-
 M.setup = function()
   cmp.setup {
     completion = {
@@ -18,38 +12,8 @@ M.setup = function()
       end,
     },
     mapping = {
-      ["<Tab>"] = cmp.mapping(function(fallback)
-        local luasnip = require "luasnip"
-        if cmp.visible() then
-          cmp.select_next_item()
-        elseif luasnip.expand_or_jumpable() then
-          luasnip.expand_or_jump()
-        elseif has_words_before() then
-          cmp.complete()
-        else
-          fallback()
-        end
-      end, {
-        "i",
-        "s",
-      }),
-      ["<S-Tab>"] = cmp.mapping(function()
-        local luasnip = require "luasnip"
-        if cmp.visible() then
-          cmp.select_prev_item()
-        elseif luasnip.jumpable(-1) then
-          luasnip.jump(-1)
-        else
-          fallback()
-        end
-      end, {
-        "i",
-        "s",
-      }),
-      ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-      ["<C-f>"] = cmp.mapping.scroll_docs(4),
-      ["<C-Space>"] = cmp.mapping.complete(),
-      ["<C-e>"] = cmp.mapping.close(),
+      ["<Tab>"] = cmp.mapping.select_next_item(),
+      ["<S-Tab>"] = cmp.mapping.select_prev_item(),
       ["<CR>"] = cmp.mapping.confirm {
         behavior = cmp.ConfirmBehavior.Insert,
         select = true,
