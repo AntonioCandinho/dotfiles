e06fda33bbd4a7df6d1aad672e12d2be175b35c4
Antonio Candinho <antonio@goodnotesapp.com>
2025-04-19 18:27:44 +0200

    Add copilot chat support

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -1,26 +1,45 @@
-local M = {}
-
-M.setup = function()
-  local copilot = require "copilot"
-
-  copilot.setup {
-    panel = {
-      enabled = true,
-      auto_refresh = true,
-    },
-    suggestion = {
-      enabled = true,
-      auto_trigger = true,
-    },
-  }
-end
-
 return {
   {
     "zbirenbaum/copilot.lua",
     cmd = "Copilot",
     build = ":Copilot auth",
     event = "InsertEnter",
-    config = M.setup,
+    opts = {
+      panel = {
+        enabled = true,
+        auto_refresh = true,
+      },
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+      },
+    },
+  },
+  {
+    {
+      "CopilotC-Nvim/CopilotChat.nvim",
+      cmd = {
+        "CopilotChatOpen",
+        "CopilotChatToggle",
+        "CopilotChatPrompts",
+        "CopilotChatCommit",
+        "CopilotChatLoad",
+        "CopilotChatExplain",
+        "CopilotChatReview",
+      },
+      dependencies = {
+        { "zbirenbaum/copilot.lua" },
+        { "nvim-lua/plenary.nvim", branch = "master" },
+      },
+      build = "make tiktoken",
+      opts = {
+        model = "claude-3.7-sonnet",
+        mappings = {
+          close = {
+            insert = "",
+          },
+        },
+      },
+    },
   },
 }
